ef0713ee29b22175d6703755bd30c95abdc34a45
Magda <>
2022-06-15 07:43:25 -0400

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -32,9 +32,27 @@ export default function Home() {
 export async function getStaticProps() {
 	const host = process.env.HOST;
 	console.log('hoooooost', host);
-	const res = await fetch(`${host}/api/data`);
-	const data = await res.json();
-
+	//const res = await fetch(`${host}/api/data`);
+	let data = [];
+	let error = "";
+	try {
+	  const res = await fetch(
+		(`${host}/api/data`),
+		{
+		  method: "GET",
+		  headers: {
+			// update with your user-agent
+			"User-Agent":
+			  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+			Accept: "application/json; charset=UTF-8",
+		  },
+		}
+	  );
+  
+	  data = await res.json();
+	} catch (e) {
+	  error = e.toString();
+	}
 	return {
 		props: {
 			data
